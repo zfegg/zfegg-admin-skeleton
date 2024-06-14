f6973cdedf14a9021ad3ec3fc632287a2ef3c16c
xiemaomao <xiemaomao520@163.com>
2024-06-14 23:34:25 +0800

    update deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import {resolve} from "path";
 import {theme} from "antd";
 // import {convertLegacyToken} from "@ant-design/compatible";
-import svgr from "@svgr/rollup";
 import {execSync} from "child_process";
-import svgImportPlugin from './scripts/svg-plugin';
+import svgr from "vite-plugin-svgr";
 
 const GIT_BRANCH = process.env.CI_COMMIT_REF_NAME || execSync('git name-rev --name-only HEAD').toString().trim()
 const GIT_HASH = (process.env.CI_COMMIT_SHA || execSync('git rev-parse HEAD').toString().trim()).substr(0, 8)
@@ -14,22 +13,6 @@ const {defaultAlgorithm, defaultSeed} = theme;
 
 const mapToken = defaultAlgorithm(defaultSeed);
 // const v4Token = convertLegacyToken(mapToken);
-const svgrPlugin: PluginOption = {
-    name: "svgr",
-    ...svgr({
-        ref: true,
-        memo: true,
-    }) as any,
-    apply: () => true
-}
-const svgPlugin: PluginOption = {
-    ...svgImportPlugin({
-        stringify: true,
-        include: [
-            /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
-        ]
-    }) as any,
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -41,8 +24,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
-        svgPlugin,
-        svgrPlugin,
+        svgr({include: '**/*.svg'})
     ],
     css: {
         preprocessorOptions: {
